4e4a5ecd89cd0bc95d479560f885b1f918d15566
Yogiski <cyogodzi@gmail.com>
2020-04-01 15:51:09 -0700

    requirements files and py 3.8 changes to handlers.py

--- handlers.py
+++ handlers.py
@@ -1,9 +1,7 @@
 import operator
 import pandas as pd
 import numpy as np
-import scipy.stats as stats
 from collections import MutableSequence
-from scipy.interpolate import interp1d
 from CanDI import data
 
 
@@ -319,7 +317,7 @@ class MutationHandler(object):
 
     def _multiple_entity_mutated(self, mut_dat, output, variant, item, translocations, fusions, all_except):
 
-        if self.version is "canc":
+        if self.version == "canc":
             variant = "gene"
         else:
             variant = "DepMap_ID"
@@ -327,7 +325,7 @@ class MutationHandler(object):
         out_dict = {"names": lambda x: list(set(x[self.by[self.version]])),
                     "dataframe": lambda x: x}
 
-        if output is "dict":
+        if output == "dict":
             out = {k:mut_dat[self.by[self.version]].loc[v].unique() for k,v in mut_dat.groupby(variant).groups.items()}
         else:
             out = out_dict[output](mut_dat)
@@ -337,68 +335,3 @@ class MutationHandler(object):
 
 
 
-class PercentagesHandler(object):
-
-    """
-    Not exactly sure how this one will work yet.
-    Definitely will calculate percentage of cell lines where a gene is transcribed/essential as well as
-    above a certain quantile.
-    """
-    pass
-
-class DESeq2(object):
-    pass
-
-
-
-
-class BinTranscription:
-
-    """
-    This might not end up getting used.
-    Could be an okay, but arbitrary way to bin things for DESeq
-    """
-
-    def __init__(self, subset, owner, name, norm=False):
-        self.subset = self._get_subset(subset, norm)
-
-    def _handle_series(self, values, style, caller):
-        knee, elbow = self._bin_transcription(values)
-
-        methods = {"over": lambda x, y: x.ge(y),
-                   "under": lambda x, y: x.le(y),
-                   "normal": lambda x, y, z: x.gt(y).lt(z)}
-
-        binned = methods[caller](values, knee)
-
-    def _quant_norm(self, array, subset):
-        """
-        array with samples in the columns and probes across the rows
-        """
-        arr = array.values
-        normed_arr = np.zeros_like(arr)
-        ranked = np.argsort(arr, axis=0)
-        normed_arr[ranked, np.arange(arr.shape[1])] = np.mean(arr[ranked, np.arange(arr.shape[1])],axis=1)[:,np.newaxis]
-        normed_df = pd.DataFrame(normed_arr, index=array.index, columns=array.columns)
-
-        return normed_df
-
-    @staticmethod
-    def _bin_transcription(array):
-
-        assert len(array) > 10
-        try:
-            pdf = stats.gaussian_kde(x).evaluate(x) #probability distribution
-        except (ValueError, np.linalg.LinAlgError):
-            return
-        cdf = np.cumsum(pdf)
-        f = interp1d(x, cdf)
-        newx = np.linspace(x.min(), x.max(), 17) #smooth curve for KneeLocator
-        y = f(newx)
-        y = y/y[-1]
-#        knee = kneed.KneeLocator(newx, y, curve='concave', direction='increasing').knee
-#        elbow = kneed.KneeLocator(newx, 1-y, curve='concave', direction='decreasing').knee
-        if self.owner is "gene":
-            setattr(self, "knee", knee)
-            setattr(self, "elbow", elbow)
-        return (knee, elbow)
