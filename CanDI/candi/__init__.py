4fd9dd077a80d8dfe98e9d8abc954f5c4a119f0e
abearab <abea@arcinstitute.org>
2024-06-06 15:08:07 -0700

    add load functino for coessentiality #45

--- CanDI/candi/__init__.py
+++ CanDI/candi/__init__.py
@@ -1,4 +1,5 @@
-from CanDI.candi import data
-data = data.Data() #Global object data instantiated on import required for access by GeneQuery Objects
-from CanDI.candi.candi import (Gene, CellLine, Organelle, Cancer, CellLineCluster, GeneCluster)
+from . import load
 
+from . import data
+data = data.Data() #Global object data instantiated on import required for access by GeneQuery Objects
+from .candi import (Gene, CellLine, Organelle, Cancer, CellLineCluster, GeneCluster)
