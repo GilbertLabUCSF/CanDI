2a6f0e505e2248a0a1d4cbbe41bdfe2472bb4d8a
abearab <abea@arcinstitute.org>
2024-06-26 03:28:44 -0700

    mend

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ subprocess.run('candi-install', shell=True)
 # -- Project information -----------------------------------------------------
 
 project = 'CanDI'
-copyright = '2021-2024 – Gilbert Lab UCSF'
+copyright = '2021-2024 – Gilbert Lab, Arc Institute / UCSF'
 author = 'Chris Yogodzinski, Abolfazl Arab'
 
 
