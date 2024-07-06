c679c6815aa9093eaf4248fd07e6414fea000a42
abearab <abea@arcinstitute.org>
2024-07-06 10:33:46 -0700

    change README format

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from CanDI.__version__ import version
 from pathlib import Path
 
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.rst").read_text()
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='PyCanDI',
